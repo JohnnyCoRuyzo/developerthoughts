5d1e87c78108f45980b9ee2afe08b564cccef5e2
Johnnatan Ruiz <nicolasruiz11@hotmail.com>
2020-10-04 19:18:21 -0500

    Updates

---
+++ precache-manifest.ce79b9dad989ab18052a09901bbf526a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea568cea28e77e3a6c8444e23800009",
+    "url": "/developerthoughts/index.html"
+  },
+  {
+    "revision": "b8d7c0d11f41232c587c",
+    "url": "/developerthoughts/static/css/2.ed890e5e.chunk.css"
+  },
+  {
+    "revision": "8ee13442daa5edaa3415",
+    "url": "/developerthoughts/static/css/main.d860b3ab.chunk.css"
+  },
+  {
+    "revision": "b8d7c0d11f41232c587c",
+    "url": "/developerthoughts/static/js/2.244860e1.chunk.js"
+  },
+  {
+    "revision": "69edab76e77a5ff14aae19f1dcd110aa",
+    "url": "/developerthoughts/static/js/2.244860e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee13442daa5edaa3415",
+    "url": "/developerthoughts/static/js/main.591dac00.chunk.js"
+  },
+  {
+    "revision": "070589284753f2f04384",
+    "url": "/developerthoughts/static/js/runtime-main.a10530b5.js"
+  },
+  {
+    "revision": "70f4932d9d44337f48472120698d7b1d",
+    "url": "/developerthoughts/static/media/Cat.70f4932d.jpg"
+  },
+  {
+    "revision": "9d7c175d9e78879c8ef61d95fd1a5a54",
+    "url": "/developerthoughts/static/media/Little_Crazy_Pets_Logo.9d7c175d.png"
+  },
+  {
+    "revision": "a42b5a6e53f6612a2e116a7d0675a220",
+    "url": "/developerthoughts/static/media/MilaImagen.a42b5a6e.jpg"
+  },
+  {
+    "revision": "e97ef24d7e861b0f2c5c74304c39f25f",
+    "url": "/developerthoughts/static/media/NegritaImagen.e97ef24d.jpg"
+  },
+  {
+    "revision": "d964b2f4ca9aac46fb100f72ca1f2a93",
+    "url": "/developerthoughts/static/media/NovaImagen.d964b2f4.jpg"
+  },
+  {
+    "revision": "366e1bee4920166374269d4be364cbcb",
+    "url": "/developerthoughts/static/media/background.366e1bee.jpg"
+  }
+]);
